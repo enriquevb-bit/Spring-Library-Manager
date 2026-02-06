d3d6186ecf4fa062e7b828afe0b05eb852edf26e
enriquevb-bit <enriquevarelabouza@gmail.com>
2026-02-06 19:58:49 +0100

    Refactorization: Changed project structure and added a new project for the auth server

---
+++ library-auth-server/src/main/java/enriquevb/libraryauthserver/LibraryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package enriquevb.libraryauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAuthServerApplication.class, args);
+    }
+
+}
---
+++ library-auth-server/src/test/java/enriquevb/libraryauthserver/LibraryAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package enriquevb.libraryauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
